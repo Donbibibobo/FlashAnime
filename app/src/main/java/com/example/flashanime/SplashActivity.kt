52949e833337450e110f877cce0c5082d4547190
Dondon <twterry10@yahoo.com.tw>
2023-10-05 21:37:58 +0800

    add splash
    add splash animation

---
+++ app/src/main/java/com/example/flashanime/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.flashanime
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import kotlin.math.log
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val logo = findViewById<ImageView>(R.id.logo)
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
