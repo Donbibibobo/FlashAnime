f28a3793e9ece5baf5c6511ed46d6df39fca7fbc
Dondon <twterry10@yahoo.com.tw>
2023-10-08 16:33:39 +0800

    modify splash
    add new logo animation

--- app/src/main/java/com/example/flashanime/SplashActivity.kt
+++ app/src/main/java/com/example/flashanime/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.example.flashanime
 
+import android.animation.Animator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AccelerateInterpolator
 import android.widget.ImageView
+import com.airbnb.lottie.LottieAnimationView
 import kotlin.math.log
 
 class SplashActivity : AppCompatActivity() {
@@ -11,13 +15,29 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val logo = findViewById<ImageView>(R.id.logo)
-        logo.alpha = 0f
-        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+        fun navigateToMainActivity(){
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }
+
+        val lottieView = findViewById<LottieAnimationView>(R.id.lottie_splash)
+        lottieView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator) {}
+            override fun onAnimationEnd(p0: Animator) {
+
+                Handler().postDelayed({
+                    lottieView.animate().scaleX(10f).scaleY(10f)
+                        .alpha(0f).setInterpolator(AccelerateInterpolator())
+                        .setDuration(1000).start()
+
+                    navigateToMainActivity()
+                }, 500)
+            }
+            override fun onAnimationCancel(p0: Animator) {}
+            override fun onAnimationRepeat(p0: Animator) {}
+        })
+
     }
 }
